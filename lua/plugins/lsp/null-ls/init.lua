412eb84bd758a8f00e83d8111f56e747685d21ca
Sascha Fleer <sascha.fleer@ecclesia-gruppe.de>
2024-03-01 07:43:04 +0100

    fix(none-ls): removed deprecated tools

--- lua/plugins/lsp/null-ls/init.lua
+++ lua/plugins/lsp/null-ls/init.lua
@@ -23,38 +23,23 @@ end
 local sources = {
   --[[ b.formatting.sqlfluff.with({ extra_args = { "--dialect", "postgres" } }), ]]
   b.formatting.shfmt,
-  b.formatting.fixjson,
-  b.formatting.ruff_format,
-  b.formatting.taplo, -- Formatting .toml files
   -- b.formatting.ngnix_beautifier,
   with_root_file(b.formatting.stylua, "stylua.toml"),
 
   -- diagnostics
-  b.diagnostics.write_good.with({ filetypes = { "md", "markdown", "markdown.pandoc", "vimwiki" } }),
+  b.diagnostics.write_good.with { filetypes = { "md", "markdown", "markdown.pandoc", "vimwiki" } },
   -- b.diagnostics.markdownlint,
-  -- b.diagnostics.ruff, -- Handled by ruff-lsp
-  -- TODO: Test if can be replaced
-  b.diagnostics.eslint_d.with({
-    condition = function(utils)
-      return utils.root_has_file(".eslintrc.js")
-    end,
-  }),
   -- with_root_file(b.diagnostics.selene, "selene.toml"),
   -- with_diagnostics_code(b.diagnostics.shellcheck),
-  b.diagnostics.shellcheck.with({ filetypes = { "sh", "zsh" } }),
-  function()
-    local utils = nls_utils.make_conditional_utils()
-
-    if utils.root_has_file(".eslintrc.js") then
-      return b.formatting.eslint_d
-    else
-      return b.formatting.prettier
-    end
-  end,
-
-  -- code actions
-  -- b.code_actions.gitsigns,
-  -- b.code_actions.gitrebase,
+  -- function()
+  --   local utils = nls_utils.make_conditional_utils()
+  --
+  --   if utils.root_has_file ".eslintrc.js" then
+  --     return b.formatting.eslint_d
+  --   else
+  --     return b.formatting.prettier
+  --   end
+  -- end,
 
   -- hover
   b.hover.dictionary,
