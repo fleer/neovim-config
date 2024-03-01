af8126532174889d36a4559e7f3defc63ac6dcc4
Sascha Fleer <sascha.fleer@ecclesia-gruppe.de>
2024-03-01 07:42:34 +0100

    feat(notebooknavigator): added jupytext

--- lua/plugins/notebooknavigator.lua
+++ lua/plugins/notebooknavigator.lua
@@ -86,4 +86,10 @@ return {
     --   nn.setup { activate_hydra_keys = "<leader>h" }
     -- end,
   },
+  {
+    "GCBallesteros/jupytext.nvim",
+    config = true,
+    -- Depending on your nvim distro or config you may need to make the loading not lazy
+    lazy = false,
+  },
 }
