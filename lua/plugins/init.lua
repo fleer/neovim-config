f4bf1a481eee3192fe215ec4f5bc60a737a418a9
Sascha Fleer <sascha.fleer@gmail.com>
2024-06-17 21:17:24 +0200

    refactor(nvimtree): removed nvimtree

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -1,10 +1,4 @@
 return {
-  -- {
-  --   "lukas-reineke/indent-blankline.nvim",
-  --   event = "BufReadPre",
-  --   config = true,
-  -- },
-  -- active indent guide and indent text objects
   {
     "echasnovski/mini.indentscope",
     version = false, -- wait till new 0.7.0 release to put it back on semver
