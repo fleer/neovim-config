372081a430ef61fcd932a024d39e18a7b41b2b00
Sascha Fleer <sascha.fleer@gmail.com>
2024-10-20 15:53:35 +0200

    refactor(lazynivim): enable matchit plugin

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -22,8 +22,6 @@ require("lazy").setup("plugins", {
     rtp = {
       disabled_plugins = {
         "gzip",
-        "matchit",
-        -- "matchparen",
         "tarPlugin",
         "tohtml",
         "tutor",
