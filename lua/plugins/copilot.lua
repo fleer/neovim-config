99eca9a059a1b80f6c7677f266ce7eb236d8945f
Sascha Fleer <sascha.fleer@ecclesia-gruppe.de>
2025-01-31 14:35:54 +0100

    refactor(copilot): switch to copilot.vim

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,33 +1,12 @@
 return {
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup {
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-        filetypes = {
-          -- markdown = true, -- overrides default
-          -- python = true,
-          sh = function()
-            if string.match(vim.fs.basename(vim.api.nvim_buf_get_name(0)), "^%.env.*") then
-              -- disable for .env files
-              return false
-            end
-            return true
-          end,
-        },
-      }
-    end,
-  },
-  {
     "CopilotC-Nvim/CopilotChat.nvim",
     branch = "main",
     dependencies = {
-      { "zbirenbaum/copilot.lua" }, -- or github/copilot.vim
+      { "github/copilot.vim" }, -- or github/copilot.vim
       { "nvim-lua/plenary.nvim" }, -- for curl, log wrapper
     },
+    build = "make tiktoken", -- Only on MacOS or Linux
     config = function()
       require("CopilotChat").setup {
         -- debug = true, -- Enable debug logging
